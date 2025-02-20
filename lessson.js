e2a4e6562cfb15f49353758c9434c3c79b89d79c
AidenH_123 <149191032+Stealthhacker123@users.noreply.github.com>
2025-02-20 00:53:11 -0500

    skibidi code

--- lessson.js
+++ lessson.js
@@ -1,6 +1,28 @@
 //-373, 63, -1964
 
 
-function lessonLibrary() {
-    
+
+
+function lessonLibrary(input) {
+    if(input == 1) {
+        
+    }
+}
+
+
+function getRandomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+let randomFloat = getRandomFloat(1, 30); 
+
+function startLesson() {
+    const num_1 = document.getElementById("num_1");
+    const num_2 = document.getElementById("num_2");
+    const userAnswer = document.getElementById("userText");
+
+    num_1.innerHTML = getRandomFloat(1, 30);
+    num_2.innterHTML = getRandomFloat(1, 30);
+
+
 }
